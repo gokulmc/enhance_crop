eb16547fabdb7140e502ce2bdb4c1033adecc274
TNTwise <tntwise3@gmail.com>
2025-02-25 20:54:58 -0600

    small patches

--- src/Util.py
+++ src/Util.py
@@ -211,22 +211,26 @@ def getCPUInfo() -> str:
     Returns the CPU information of the system.
     """
     # return platform.processor() + " " + str(psutil.cpu_count(logical=False)) + " cores" + platform.
-    if PLATFORM == "win32":
-        try:
-            # Run the 'wmic' command to get CPU information
-            result = subprocess.run(
-                ["wmic", "cpu", "get", "name"],
-                capture_output=True,
-                text=True,
-                check=True,
-            )
-            # Split the result by lines and return the second line which contains the CPU name
-            return result.stdout.split("\n")[2].strip()
-        except Exception as e:
-            print(f"An error occurred while getting CPU brand: {e}")
-            return "X86_64 CPU"
-    else:
-        return cpuinfo.get_cpu_info()["brand_raw"]
+    try:
+        if PLATFORM == "win32":
+            try:
+                # Run the 'wmic' command to get CPU information
+                result = subprocess.run(
+                    ["wmic", "cpu", "get", "name"],
+                    capture_output=True,
+                    text=True,
+                    check=True,
+                )
+                # Split the result by lines and return the second line which contains the CPU name
+                return result.stdout.split("\n")[2].strip()
+            except Exception as e:
+                print(f"An error occurred while getting CPU brand: {e}")
+                return "X86_64 CPU"
+        else:
+            return cpuinfo.get_cpu_info()["brand_raw"]
+    except Exception as e:
+        log(f"An error occurred while getting CPU information: {e}")
+        return "Unknown"
 
 
 def copy(prev: str, new: str):
