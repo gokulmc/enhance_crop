c9ecf68d1c164a68e1ca41a9302ea7d1d69e6e0a
TNTwise <tntwise3@gmail.com>
2025-03-02 16:41:31 -0600

    have frontend handle cwd

--- backend/rve-backend.py
+++ backend/rve-backend.py
@@ -6,24 +6,15 @@ os.environ["HCC_AMDGPU_TARGET"] = "gfx1100"
 
 import argparse
 import sys
-from src.RenderVideo import Render
-from src.version import __version__
-from src.utils.Util import (
-    checkForPytorchCUDA,
-    checkForPytorchROCM,
-    checkForNCNN,
-    checkForTensorRT,
-    check_bfloat16_support,
-    checkForDirectML,
-    checkForDirectMLHalfPrecisionSupport,
-    get_gpus_ncnn,
-    get_gpus_torch,
-)
 
+from src.constants import set_manual_cwd
 
 class HandleApplication:
     def __init__(self):
         self.args = self.handleArguments()
+        if self.args.cwd:
+            
+            set_manual_cwd(self.args.cwd)
         if not self.args.list_backends:
             self.checkArguments()
             if not self.batchProcessing():
@@ -56,6 +47,20 @@ class HandleApplication:
         half_prec_supp = False
         availableBackends = []
         printMSG = ""
+        
+        
+        from src.utils.Util import (
+            checkForPytorchCUDA,
+            checkForPytorchROCM,
+            checkForNCNN,
+            checkForTensorRT,
+            check_bfloat16_support,
+            checkForDirectML,
+            checkForDirectMLHalfPrecisionSupport,
+            get_gpus_ncnn,
+            get_gpus_torch,
+            log
+        )
 
         if checkForTensorRT():
             """
@@ -114,6 +119,7 @@ class HandleApplication:
         print(printMSG)
 
     def renderVideo(self):
+        from src.RenderVideo import Render
         Render(
             # model settings
             inputFile=self.args.input,
@@ -428,6 +434,7 @@ class HandleApplication:
 
     def checkArguments(self):
         if self.args.version:
+            from src.version import __version__
             print(f"{__version__}")
             sys.exit(0)
         if (
