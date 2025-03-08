9c0d6dddbc7ffcbd1ac93031aa37d568ecdf7989
TNTwise <77559100+TNTwise@users.noreply.github.com>
2025-03-07 16:07:23 -0800

    Update version.py

--- backend/src/version.py
+++ backend/src/version.py
@@ -1,2 +1,2 @@
-__version__ = "2.2.5-dev03022025" # this is the version of the backend, this is compared to the version of the front end. need to be the same
+__version__ = "2.2.5-dev03052025" # this is the version of the backend, this is compared to the version of the front end. need to be the same
 
