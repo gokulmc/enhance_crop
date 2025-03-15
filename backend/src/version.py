87a40aed023f9e98a2c215c40991d653f2ab5693
tntwise <tntwise3@gmail.com>
2025-03-14 19:56:35 -0500

    update version

--- backend/src/version.py
+++ backend/src/version.py
@@ -1,2 +1,2 @@
-__version__ = "2.2.5-dev03122025" # this is the version of the backend, this is compared to the version of the front end. need to be the same
+__version__ = "2.2.5-dev03142025" # this is the version of the backend, this is compared to the version of the front end. need to be the same
 
--- src/version.py
+++ src/version.py
@@ -1,2 +1,2 @@
 version = "2.2.5"
-backend_dev_version = "2.2.5-dev03122025"  # has to match version of backend, update this wehenver updating pre release
+backend_dev_version = "2.2.5-dev03142025"  # has to match version of backend, update this wehenver updating pre release
