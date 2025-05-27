af61de9f8133c5b28b9bac5f4b1e4a2a4a8f1f24
tntwise <tntwise3@gmail.com>
2025-05-27 18:50:43 -0500

    fix ncnn render

--- backend/src/utils/Util.py
+++ backend/src/utils/Util.py
@@ -123,11 +123,11 @@ def resize_image_bytes(image_bytes: bytes, width: int, height: int, target_width
     Returns:
         bytes: The resized image in bytes.
     """
-    log(f"Resizing image from {width}x{height} to {target_width}x{target_height}")
     if target_width == width and target_height == height:
         return image_bytes
     channels = len(bytes(image_bytes)) / (height * width) # 3 if RGB24/SDR, 6 if RGB48/HDR
-    dtype = np.uint8 if channels == 3 else np.uint16
+    dtype = np.uint8 if channels == 3  else np.uint16
+    log(f"Resizing image from {width}x{height} to {target_width}x{target_height} with dtype {dtype}")
     # Convert bytes to numpy array
     if target_width < width or target_height < height:
         # Best for downscaling
