b5218a1b715f55c2c50b5d9eaab77ca5922d3ddf
TNTwise <77559100+TNTwise@users.noreply.github.com>
2025-06-09 12:48:07 -0700

    Update UpscaleTorch.py

--- backend/src/pytorch/UpscaleTorch.py
+++ backend/src/pytorch/UpscaleTorch.py
@@ -111,15 +111,16 @@ class UpscalePytorch:
         self.trt_aux_streams = trt_max_aux_streams
         self.trt_debug = trt_debug
         self.trt_static_shape = trt_static_shape
-        self.hdr_mode = hdr_mode
+        self.hdr_mode = hdr_mode         
 
-        if width <= 3840 and height <= 3840 and width > 1920 and height > 1920:
-            self.trt_min_shape = [1920, 1920]
+        if width <= 3840 and height <= 3840 and (width > 1920 or height > 1920):
+            self.trt_min_shape = [1920, height] if height < width else [width, 1920]
             self.trt_opt_shape = [3840, 2160]
             self.trt_max_shape = [3840, 3840]
-        
-        if width <= 1920 and height <= 1920 and width >= 128 and height >= 128:
-            self.trt_min_shape = [128, 128]
+
+        if width <= 1920 and height <= 1920 and (width >= 128 or height >= 128):
+
+            self.trt_min_shape = [1920, height] if height < width else [width, 1920]
             self.trt_opt_shape = [1920, 1080]
             self.trt_max_shape = [1920, 1920]
         
