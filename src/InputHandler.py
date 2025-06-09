6d3138d650df5a237726dfac8ef15cc75031646b
tntwise <tntwise3@gmail.com>
2025-06-08 19:00:30 -0500

    remove unused code

--- src/InputHandler.py
+++ src/InputHandler.py
@@ -60,41 +60,6 @@ class FFMpegInfoWrapper:
             return codec.groups()[0]
         return "unknown"
 
-"""    
-class VideoLoader:
-    def __init__(self, inputFile):
-        self.inputFile = inputFile
-
-    def loadVideo(self):
-        log(f"Loading video file: {self.inputFile}")
-        self.capture = cv2.VideoCapture(self.inputFile, cv2.CAP_FFMPEG)
-
-    def isValidVideo(self):
-        log(f"Checking if video file is valid: {self.inputFile}")
-        disabled_extensions = ["txt", "jpg", "jpeg", "png", "bmp", "webp"]
-        file_extension = self.inputFile.split(".")[-1].lower()
-        return self.capture.isOpened() and \
-               self.capture.get(cv2.CAP_PROP_FRAME_COUNT) > 1 and \
-               file_extension not in disabled_extensions
-
-    def getData(self):
-        self.width = int(self.capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.height = int(self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        self.bitrate = int(self.capture.get(cv2.CAP_PROP_BITRATE))
-        self.videoContainer = self.inputFile.split(".")[-1]
-        codec = int(self.capture.get(cv2.CAP_PROP_FOURCC))
-        self.codec_str = (
-            chr(codec & 0xFF)
-            + chr((codec >> 8) & 0xFF)
-            + chr((codec >> 16) & 0xFF)
-            + chr((codec >> 24) & 0xFF)
-        )
-        self.fps = self.capture.get(cv2.CAP_PROP_FPS)
-        self.total_frames = int(self.capture.get(cv2.CAP_PROP_FRAME_COUNT))
-        self.fps = self.capture.get(cv2.CAP_PROP_FPS)
-        self.duration = self.total_frames / self.fps
-        self.capture.release()"""
-
 class VideoLoader:
     def __init__(self, inputFile):
         self.inputFile = inputFile
