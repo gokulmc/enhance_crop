53ca4983adf1ab5af90dd35dcab7734d2d595825
tntwise <tntwise3@gmail.com>
2025-06-26 10:13:53 -0700

    re enable old engine deletion

--- src/version.py
+++ src/version.py
@@ -1,2 +1,2 @@
 version = "2.3.4"
-backend_dev_version = "2.3.4-dev20250616"  # has to match version of backend, update this wehenver updating pre release
+backend_dev_version = "2.3.4"  # has to match version of backend, update this wehenver updating pre release
