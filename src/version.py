342bb3c292a03063ec4b9532fc5dd4458ed0c07f
TNTwise <tntwise3@gmail.com>
2025-07-06 07:55:14 -0700

    update version

--- src/version.py
+++ src/version.py
@@ -1,2 +1,2 @@
 version = "2.3.5"
-backend_dev_version = "2.3.5-dev06262025"  # has to match version of backend, update this wehenver updating pre release
+backend_dev_version = "2.3.5-dev07062025"  # has to match version of backend, update this wehenver updating pre release
