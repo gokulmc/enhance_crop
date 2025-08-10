2144c34fb3bb492b78b1ce5f109506c6bd8c75f8
tntwise <tntwise3@gmail.com>
2025-08-10 10:54:33 -0700

    fix launching because no requests in backend

--- backend/src/utils/GetFFMpeg.py
+++ backend/src/utils/GetFFMpeg.py
@@ -1,12 +1,12 @@
 from ..constants import FFMPEG_PATH, CPU_ARCH, PLATFORM, CWD
 import os
+import sys
 from .FileHandler import FileHandler
-import requests
 download_path = os.path.join(CWD, "ffmpeg")
 installed_path = FFMPEG_PATH
 
 def download_ffmpeg():
-    if not os.path.exists(installed_path):
+    if not os.path.isfile(installed_path):
         link = "https://github.com/TNTwise/real-video-enhancer-models/releases/download/models/"
         match PLATFORM:
             case "linux":
@@ -15,16 +15,22 @@ def download_ffmpeg():
                 link += "ffmpeg.exe" if CPU_ARCH == "x86_64" else "ffmpeg-windows-arm64.exe"
             case "darwin":
                 link += "ffmpeg-macos-bin" if CPU_ARCH == "x86_64" else "ffmpeg-macos-arm"
+        
+        try:
+            import requests
+            print("Downloading FFMpeg from " + link)
+            
+            response = requests.get(link, stream=True)
+            total_size = int(response.headers.get("content-length", 0))
+            block_size = 1024  
+            with open(download_path, "wb") as file:
+                for data in response.iter_content(block_size):
+                    file.write(data)
+            print("Download completed.")
 
-        print("Downloading FFMpeg from " + link)
-        response = requests.get(link, stream=True)
-        total_size = int(response.headers.get("content-length", 0))
-        block_size = 1024  
-        with open(download_path, "wb") as file:
-            for data in response.iter_content(block_size):
-                file.write(data)
-        print("Download completed.")
-
-        FileHandler.createDirectory(os.path.dirname(installed_path))
-        FileHandler.moveFile(download_path, installed_path)
-        FileHandler.makeExecutable(installed_path)
+            FileHandler.createDirectory(os.path.dirname(installed_path))
+            FileHandler.moveFile(download_path, installed_path)
+            FileHandler.makeExecutable(installed_path)
+        except Exception as e:
+            print(f"Error downloading FFMpeg: {e}", file=sys.stderr)
+            
