6cae0b0323cd113b2a4be59dca55c85209bb592f
TNTwise <tntwise3@gmail.com>
2025-08-10 00:25:12 -0500

    update demo screenshot

--- backend/src/FFmpegBuffers.py
+++ backend/src/FFmpegBuffers.py
@@ -38,10 +38,10 @@ class FFmpegRead(Buffer):
         if self.hdr_mode:
             self.inputFrameChunkSize = width * height * 6
         else:
-            if self.input_pixel_format == "yuv420p":
+            """if self.input_pixel_format == "yuv420p":
                 self.inputFrameChunkSize = width * height * 3 // 2
-            else:
-                self.inputFrameChunkSize = width * height * 3
+            else:"""
+            self.inputFrameChunkSize = width * height * 3
 
         self.readProcess = subprocess_popen_without_terminal(
             self.command(),
@@ -73,8 +73,8 @@ class FFmpegRead(Buffer):
             "-f",
             "image2pipe",
             "-pix_fmt",
-            "rgb48le" if self.hdr_mode else (self.input_pixel_format if self.input_pixel_format == "yuv420p" else "rgb24"),
-            #"rgb48le" if self.hdr_mode else "rgb24",
+            #"rgb48le" if self.hdr_mode else (self.input_pixel_format if self.input_pixel_format == "yuv420p" else "rgb24"),
+            "rgb48le" if self.hdr_mode else "rgb24",
             "-vcodec",
             "rawvideo",
             "-s",
@@ -95,7 +95,7 @@ class FFmpegRead(Buffer):
         if len(chunk) < self.inputFrameChunkSize:
             return None
         
-        if self.input_pixel_format == "yuv420p":
+        """if self.input_pixel_format == "yuv420p":
             # Convert raw YUV420p data to RGB
             # The data is Y plane, then U plane, then V plane, concatenated.
             # cv2.COLOR_YUV420P2RGB expects a single channel image of shape (height * 3 // 2, width)
@@ -104,8 +104,8 @@ class FFmpegRead(Buffer):
             yuv_image_height = self.height * 3 // 2
             yuv_image = np_frame.reshape((yuv_image_height, self.width))
             rgb_image = cv2.cvtColor(yuv_image, cv2.COLOR_YUV2RGB_I420)
-            # cv2.imwrite("temp_rgb_image.png", rgb_image)  # Debugging line, can be removed
-            chunk = rgb_image.tobytes()
+            cv2.imwrite("temp_rgb_image.png", rgb_image)  # Debugging line, can be removed
+            chunk = rgb_image.tobytes()"""
         
         return chunk
 
