2cb1fba46a6d4411966edcf466c601ff1256c856
tntwise <tntwise3@gmail.com>
2025-09-08 13:35:22 -0500

    fix versioning

--- src/version.py
+++ src/version.py
@@ -1,2 +1,2 @@
 version = "2.3.7"
-backend_dev_version = "2.3.7-09"  # has to match version of backend, update this wehenver updating pre release
+backend_dev_version = "2.3.7-dev09"  # has to match version of backend, update this wehenver updating pre release
